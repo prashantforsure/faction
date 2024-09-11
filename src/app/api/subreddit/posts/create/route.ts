68df90b7a8e1edb03c31d585d9d1995cae2994f4
prashantforsure <prashantforsure@gmail.com>
2024-09-11 08:54:43 +0530

    post created

---
+++ src/app/api/subreddit/posts/create/route.ts
@@ -0,0 +1,44 @@
+import { getAuthSession } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { PostValidator } from '@/lib/validators/post'
+import { NextRequest, NextResponse } from 'next/server'
+import { z } from 'zod'
+
+
+export async function POST(req: NextRequest){
+    try{
+        const body = await req.json();
+        const { title, content, subredditId } = PostValidator.parse(body)
+        const session = await getAuthSession();
+
+        if(!session?.user){
+            return NextResponse.json({
+                message: " unauthorised"
+            },{
+                status: 403
+            })
+        }
+        const subscription = await db.subscription.findFirst({
+            where: {
+                subredditId,
+                //@ts-ignore
+                userId: session.user.id,
+                
+            }
+        })
+        if(!subscription){
+            return NextResponse.json({
+                message: "user is not subscribed"
+            })
+        }
+        await db.post.create({
+            data: {
+                title,
+                content,
+                //@ts-ignore
+                authorId: session.user.id,
+                subredditId,
+            }
+        })
+    }
+}
