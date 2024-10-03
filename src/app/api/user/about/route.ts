e9bb98f7a9233af53a375a62383d35b9db0077ae
prashantforsure <prashantforsure@gmail.com>
2024-10-03 20:20:24 +0530

    user api

--- src/app/api/user/about/route.ts
+++ src/app/api/user/about/route.ts
@@ -36,5 +36,18 @@ try{
                 about: about
             }
         })
+        return NextResponse.json({
+            updateAbout
+        },{
+            status: 200
+        }
+    )
+}catch(error){
+    console.error(error);
+    return NextResponse.json({
+        message: "Something went wrong"
+    }, {
+        status: 500
+    });
 }
 }
